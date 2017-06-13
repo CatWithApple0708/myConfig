f504931c98891fb044a105fdce035409575eae70
赵贺 <1013909206@qq.com>
2017-06-13 12:33:06 +0800

    N

--- install_basic.sh
+++ install_basic.sh
@@ -3,14 +3,6 @@
 #抽空完成这个分类 然后将不同的程序放在不同的脚本里面
 # test privage
 # This is basic install
-sudo touch /install_file_1013909206_tmp_for_testing_privage
-if [ $? -eq 0 ];then
-	sudo rm /install_file_1013909206_tmp_for_testing_privage
-else
-	echo "Please type sudo ./install.sh"
-	exit 1
-fi
-
 if [ $MyConfigDir ];then
 	echo $MyConfigDir
 else
@@ -36,6 +28,3 @@ echo -e "source $MyConfigDir/config.sh" >> ~/.bashrc
 # vimrc config
 echo "install vim_runtime"
 source ./src/vim_runtime/install_awesome_vimrc.sh 
-
-
-
