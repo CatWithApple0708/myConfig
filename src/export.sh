f17af294e1102e966da400bda290203514536628
zhaohe <1013909206@qq.com>
2017-06-22 15:50:49 +0800

    add Java Path

--- src/export.sh
+++ src/export.sh
@@ -2,6 +2,9 @@ export PATH=$PATH:/usr/local/arm/current_compiler/bin
 export KERNEL=/home/zhaohe/cspace/rk3128_Zhaohe/svn/rk3128/kernel_yuan
 export TOOLS=$MyConfigPath/src/bin/
 
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
+export JRE_HOME=$JAVA_HOME/jre
+export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib
 
-export JRE_HOME=/usr/lib/jvm/java-1.7.0-openjdk-amd64/jre
+
 export LD_LIBRARY_PATH=.
